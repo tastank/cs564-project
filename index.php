5e37f84792f14366e8861cf12ff5471041ba77fd
Tyler Stank <tbear2500@gmail.com>
2015-02-12 13:10:57 -0600

    Included conf file

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    include("conf/conf.php");
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
@@ -5,5 +8,7 @@
 <title>Project root</title>
 </head>
 <body>
+<?php
+?>
 </body>
 </html>
