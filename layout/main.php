a33c71d3bb82dbf83f13df666f7659d42eb74f8e
Tyler A Stank <tbear2500@gmail.com>
2015-02-20 12:36:01 -0600

    Changed paths

---
+++ layout/main.php
@@ -0,0 +1,19 @@
+<?php include(__DIR__."/../conf/conf.php"); ?>
+<link rel="stylesheet" href="<?php echo SITE_ROOT ?>/conf/style_main.css" type="text/css" media="all" />
+
+
+<?php
+
+    function print_menu() {
+        ?>
+            <div id="horiz_navbar">
+                <ul>
+                    <li><a href="">View flights</a></li>
+                    <li><a href="">Scheduler</a></li>
+                    <li><a href="">Log in</a></li>
+                </ul>
+            </div>
+        <?php
+    }
+
+?>
