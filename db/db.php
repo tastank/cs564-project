285b157dbba9ce2b3de19753f45e3758bccc4fed
Tyler A Stank <tbear2500@gmail.com>
2015-04-22 15:42:53 -0500

    Initial commit

---
+++ db/db.php
@@ -0,0 +1,11 @@
+<?php
+    $dbhost = "localhost";
+    $dbname = "fbo";
+    $dbuser = "fboadmin";
+    $dbpass = "nimdaobf";
+    $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+//  mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
+//  mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
+
+?>
