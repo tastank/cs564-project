118d89f1ce920d356ca2d52e32463f5c270914c6
Tyler A Stank <tbear2500@gmail.com>
2015-04-22 15:48:38 -0500

    Used functions for modularisation!

--- index.php
+++ index.php
@@ -1,14 +1,9 @@
 <?php
     include(__DIR__."/conf.php");
     include(__DIR__."/layout/main.php");
+    include_once(__DIR__."/layout/page.php");
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
-<head>
-<title>Project root</title>
-</head>
-<body>
+page_header("Project root");
 <?php
     print_menu();
 ?>
