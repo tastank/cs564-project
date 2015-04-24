fe4d2b581f05ce8b0b16d3ad1f6a8815bf3cd1de
Tyler A Stank <tbear2500@gmail.com>
2015-04-24 15:36:15 -0500

    Added password retrieval function; should probably be moved elsewhere

--- db/db.php
+++ db/db.php
@@ -3,9 +3,23 @@
     $dbname = "fbo";
     $dbuser = "fboadmin";
     $dbpass = "nimdaobf";
-    $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+    $db = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+    if ($db->connect_errno) {
+        printf("Connection failed: %s\n", $db->connect_error);
+        exit();
+    }
 
 //  mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
 //  mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
+
+    function get_password_from_db($username) {
+        $password_query = "SELECT password FROM Pilot WHERE username='" . $username . "';";
+        $result = db->query($password_query);
+        if (!$result || $result->num_rows != 1) {
+            return false;
+        }
+        return $result->fetch_assoc()['password'];
+    }
 
 ?>
