1c91575ba3b3f2dc4242718c95e3cd80b20596f8
Tyler A Stank <tbear2500@gmail.com>
2015-04-24 15:20:55 -0500

    Partially implemented logging in

--- login.php
+++ login.php
@@ -1,5 +1,36 @@
 <?php
     include_once(__DIR__."/conf.php");
+    include(__DIR__."/layout/main.php");
+    include_once(__DIR__."/layout/page.php");
 
     page_header("Log in");
+    print_menu();
+
+    if (isset($_POST['username'])) {
+        //a user is trying to log in
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        //SELECT password FROM Pilot WHERE username = $username;
+        $password_from_db = password_hash("alpha", PASSWORD_BCRYPT);
+
+
+        if (password_verify($password, $password_from_db)) {
+            echo "Login success!";
+        } else {
+            echo "Login failure.";
+        }
+    }
+
+?>
+<br /><br />
+<div class="pass_warning">Do not use a password you would use anywhere else; it will be sent as plaintext.</div>
+
+<form method="POST" action="./login.php">
+Username: <input type="text" name="username" /><br />
+Password: <input type="password" name="password" />
+<input type="submit" name="submit" value="Submit" />
+</form>
+
+<?php
+    page_footer();
 ?>
