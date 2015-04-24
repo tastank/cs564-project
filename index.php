307935f0474b65f4890d50968b1d731601f4d956
Tyler A Stank <tbear2500@gmail.com>
2015-04-24 14:49:16 -0500

    Fixed page header

--- index.php
+++ index.php
@@ -2,9 +2,8 @@
     include(__DIR__."/conf.php");
     include(__DIR__."/layout/main.php");
     include_once(__DIR__."/layout/page.php");
-?>
-page_header("Project root");
-<?php
+
+    page_header("Project root");
     print_menu();
 ?>
 </body>
