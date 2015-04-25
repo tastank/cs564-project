b820e9ec7708e4e72c931f314a6d666c5830bc50
Tyler A Stank <tbear2500@gmail.com>
2015-04-25 16:39:33 -0500

    Fixed errors and login status message conditions

--- login.php
+++ login.php
@@ -4,7 +4,6 @@
     include_once(__DIR__."/layout/page.php");
 
     page_header("Log in");
-    print_menu();
 
     if (isset($_POST['username'])) {
         //a user is trying to log in
@@ -14,14 +13,18 @@
         $password_from_db = get_password_from_db($username);
 
         if (password_verify($password, $password_from_db)) {
-            echo "Login success!";
+            //todo: replace this with a login($username) function
             $_SESSION['username'] = $username;
+            echo "Login success!";
         } else {
             echo "Login failure.";
         }
     }
 
+    print_menu();
+
 ?>
+
 <br /><br />
 <div class="pass_warning">Do not use a password you would use anywhere else; it will be sent as plaintext.</div>
 
