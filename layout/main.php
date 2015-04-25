bde1b865351fd427109160e2d73a6af6c8fe057f
Tyler A Stank <tbear2500@gmail.com>
2015-04-25 17:22:39 -0500

    Changed login status check for better organization

--- layout/main.php
+++ layout/main.php
@@ -10,13 +10,13 @@
                 <ul>
                     <li><a href="">View flights</a></li>
                     <li><a href="">Scheduler</a></li>
-                    <?php if (!isset($_SESSION['username'])) {
+                    <?php if (!is_logged_in()) {
                         ?>
                     <li><a href="<?php echo SITE_ROOT ?>/login.php">Log in</a></li>
                     <?php } else {
                         ?>
                     <li><a href="">Account</a></li>
-                    <li><a href="<?php echo SITE_ROOT ?>/logout.php">Log out</a></li>
+                        <li><a href="<?php echo SITE_ROOT ?>/logout.php">Log out</a></li>
                     <?php } ?>
                 </ul>
             </div>
