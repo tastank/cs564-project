507f723333e3ab754ea5bec5ff794840ff819bd3
Tyler Stank <tbear2500@gmail.com>
2015-05-01 15:12:30 -0500

    Added redirect if not logged in or not admin

--- admin.php
+++ admin.php
@@ -3,6 +3,11 @@
     include(__DIR__."/layout/main.php");
     include_once(__DIR__."/layout/page.php");
 
+    //Don't display if not logged in or not admin!
+    if (!is_logged_in() || !is_admin()) {
+        header('Location: '.SITE_ROOT.'/index.php');
+    }
+
     page_header("Project root");
     print_menu();
 
