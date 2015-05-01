26af56ad082cad50081a0253b2aba342b42a5b45
Tyler Stank <tbear2500@gmail.com>
2015-05-01 16:22:04 -0500

    Added auth/deauth section (nonfunctional) with lots of placeholders

--- admin.php
+++ admin.php
@@ -14,5 +14,50 @@
 //TODO: This is where administrator can modify accounts, authorize rentals,
 //  add or change aircraft records, and add or change type records
 
+    //placeholder
+    function get_all_usernames() {
+        $users = array();
+        array_push($users, "tstank");
+        array_push($users, "tyler");
+        array_push($users, "asdf");
+        return $users;
+    }
+
+    //also placeholder
+    function get_all_reg_numbers() {
+        $numbers = array();
+        array_push($numbers, "N91HL");
+        array_push($numbers, "N6146Q");
+        array_push($numbers, "N95787");
+        array_push($numbers, "N49439");
+        return $numbers;
+    }
+
+    //Authorize a rental
+    $users = get_all_usernames();
+    $reg_numbers = get_all_reg_numbers();
+
+    $user_dropdown = "";
+    $aircraft_dropdown = "";
+    foreach ($users as $user) {
+		$user_dropdown .= '<option value="' . $user . '">' . $user . '</option>';
+    }
+
+    foreach ($reg_numbers as $reg_num) {
+		$aircraft_dropdown .= '<option value="' . $reg_num . '">' . $reg_num . '</option>';
+    }
+
+    ?>
+
+    <h2> Authorize or deauthorize renters </h2>
+    <form method="POST" action="auth.php">
+	<span> User: </span><select id="pilot"><?php echo $user_dropdown?></select>
+	<span> Aircraft: </span><select id="pilot"><?php echo $aircraft_dropdown?></select><br />
+    <input type="radio" name="auth_type" value="add" />Authorize<br />
+    <input type="radio" name="auth_type" value="remove" />Deauthorize<br />
+    <input type="submit" name="submit" value="Submit" />
+    </form>
+
+<?php
     page_footer();
 ?>
