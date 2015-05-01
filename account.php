787a4377fffbe3199faf7a3a1dbc20200464564e
andrewedstrom <andrewedstrom@gmail.com>
2015-05-01 16:00:25 -0500

    Added account page, updated user

--- account.php
+++ account.php
@@ -3,11 +3,38 @@
     include(__DIR__."/layout/main.php");
     include_once(__DIR__."/layout/page.php");
 
-    page_header("Project root");
+    page_header("Account");
+    
+    $form_name = get_name();
+    $form_phone = get_phone();
+    $form_address = get_address();
+    //$form_password = "";
+    
     print_menu();
 
 //TODO: This is where the user can update their personal information (name,
-//  phone, address, password, username
+//  phone, address, password,
 
+    //update phone number    
+?>
+
+
+    
+    
+    Enter new account details
+    <form method="POST" action="./account.php">
+     <!--TODO: use a table or some css to make the form look nicer-->
+     Name: <input type="text" name="name" value="<?php echo $form_name ?>"/><br />
+     Phone: <input type="text" name="phone" value="<?php echo $form_phone ?>"/><br />
+     Address: <input type="text" name="address" value="<?php echo $form_address ?>"/><br />
+     <input type="submit" name="submit" value="Submit" />
+     </form>
+     
+<!-- 
+     Separate these into different forms.
+     TODO: Add password modification
+ -->
+    
+<?php
     page_footer();
 ?>
--- conf/user.php
+++ conf/user.php
@@ -4,6 +4,66 @@
         return isset($_SESSION['username']);
     }
 
+    function get_username() {
+        if (!is_logged_in()) {
+            return null;
+        }
+        return $_SESSION['username'];
+    }
+    
+    function get_name() {
+        global $db;
+        if (!is_logged_in()) {
+            return null;
+        }
+        $username = get_username();
+        $name_query = "SELECT name FROM Pilot WHERE username='" . $username . "';";
+        $result = $db->query($name_query);
+        $name = $result->fetch_assoc()['name'];
+        $result->close();
+        return $name;
+        
+    }
+    
+    function get_phone() {
+        global $db;
+        if (!is_logged_in()) {
+            return null;
+        }
+        $username = get_username();
+        $phone_query = "SELECT phone FROM Pilot WHERE username='" . $username . "';";
+        $result = $db->query($phone_query);
+        $phone = $result->fetch_assoc()['phone'];
+        $result->close();
+        return $phone;
+    }
+    
+    function get_address() {
+        global $db;
+        if (!is_logged_in()) {
+            return null;
+        }
+        $username = get_username();
+        $address_query = "SELECT address FROM Pilot WHERE username='" . $username . "';";
+        $result = $db->query($address_query);
+        $address = $result->fetch_assoc()['address'];
+        $result->close();
+        return $address;
+    }
+
+    function is_admin() {
+        global $db;
+        $username = get_username();
+        $password_query = "SELECT is_admin FROM Pilot WHERE username='" . $username . "';";
+        $result = $db->query($password_query);
+        if (!$result || $result->num_rows != 1) {
+            return false;
+        }
+        $is_admin = $result->fetch_assoc()['is_admin'];
+        $result->close();
+        return $is_admin;
+    }
+
     function get_password_from_db($username) {
         global $db;
         $username = $db->real_escape_string($username);
@@ -22,7 +82,9 @@
         $username = $db->real_escape_string($username);
         $username_query = "SELECT name FROM Pilot WHERE username='" . $username . "';";
         $result = $db->query($username_query);
-        if ($result->num_rows != 0) {
+        if ($result === false) {
+            return false;
+        } else if ($result->num_rows != 0) {
             $result->close();
             return true;
         } else {
