c7532e9b9a1a25c1ee21f037ca738be797e375d2
Tyler Stank <tbear2500@gmail.com>
2015-05-04 14:20:46 -0500

    Changed session message variable names

--- aircraftrecords.php
+++ aircraftrecords.php
@@ -5,25 +5,25 @@
 
     //change this condition
     /*if (!isset($_POST['aircraft']) || (!isset($_POST['cost']) && !isset($_POST['delete']))) {
-        $_SESSION['record_err'] = "Something is horribly wrong, or you've forgotten to set a field.";
+        $_SESSION['aircraft_record_err'] = "Something is horribly wrong, or you've forgotten to set a field.";
         // Can safely do this, as the admin page will redirect to login if not admin
         header('Location: '.SITE_ROOT.'/admin.php');
     }*/
     if (isset($_POST['delete'])) {
         $aircraft = $_POST['aircraft'];
         if (delete_record($aircraft)) {
-            $_SESSION['record_msg'] = "Deletion successful";
+            $_SESSION['aircraft_record_msg'] = "Deletion successful";
         } else {
-            $_SESSION['record_err'] = "Deletion unsuccessful";
+            $_SESSION['aircraft_record_err'] = "Deletion unsuccessful";
         }
     }
     if (isset($_POST['changerecord'])) {
         $aircraft = $_POST['aircraft'];
         $cost = $_POST['cost'];
         if (update_cost($aircraft, $cost)) {
-            $_SESSION['record_msg'] = "Update successful";
+            $_SESSION['aircraft_record_msg'] = "Update successful";
         } else {
-            $_SESSION['record_err'] = "Update unsuccessful";
+            $_SESSION['aircraft_record_err'] = "Update unsuccessful";
         }
     }
     if (isset($_POST['addrecord'])) {
@@ -31,9 +31,9 @@
         $tid = $_POST['type'];
         $cost = $_POST['cost'];
         if (add_aircraft($reg_number, $cost, $tid)) {
-            $_SESSION['add_msg'] = "Addition successful";
+            $_SESSION['aircraft_add_msg'] = "Addition successful";
         } else {
-            $_SESSION['add_err'] = "Addition unsuccessful";
+            $_SESSION['aircraft_add_err'] = "Addition unsuccessful";
         }
     }
     header('Location: '.SITE_ROOT.'/admin.php');
