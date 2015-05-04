65ba0970b64dd9fb2d66023c80a7f7025a49488f
Tyler Stank <tbear2500@gmail.com>
2015-05-04 16:15:57 -0500

    Initial commit

---
+++ db/type.php
@@ -0,0 +1,108 @@
+<?php
+    include_once(__DIR__."/db.php");
+
+    function get_all_full_types() {
+        global $db;
+        $type_query = "SELECT manf, number, common_name, short_name FROM Type";
+        $result = $db->query($type_query);
+        if ($result === false) {
+            return null;
+        }
+        $types = array();
+        while ($row = $result->fetch_assoc()) {
+            array_push($types, $row['manf'] . " " . $row['number'] . " " . $row['common_name'] . " (" . $row['short_name'] . ")");
+        }
+        $result->close();
+        return $types;
+    }
+
+    function get_all_full_type_names() {
+        global $db;
+        $type_query = "SELECT manf, number, common_name FROM Type";
+        $result = $db->query($type_query);
+        if ($result === false) {
+            return null;
+        }
+        $types = array();
+        while ($row = $result->fetch_assoc()) {
+            array_push($types, $row['manf'] . " " . $row['number'] . " " . $row['common_name']);
+        }
+        $result->close();
+        return $types;
+    }
+
+    function get_all_tids() {
+        global $db;
+        $tid_query = "SELECT tid FROM Type";
+        $result = $db->query($tid_query);
+        if ($result === false) {
+            return null;
+        }
+        $tids = array();
+        while ($row = $result->fetch_assoc()) {
+            array_push($tids, $row['tid']);
+        }
+        $result->close();
+        return $tids;
+    }
+
+    function delete_type($tid) {
+        // Should probably also delete all aircraft of that type.  But...
+        global $db;
+        $tid = $db->real_escape_string($tid);
+        $delete_query = "DELETE FROM Type WHERE tid=" . $tid . ";";
+
+        return $db->query($delete_query);
+    }
+
+    function add_type($manf, $number, $common_name, $short_name) {
+        global $db;
+        $manf = $db->real_escape_string($manf);
+        $number = $db->real_escape_string($number);
+        $common_name = $db->real_escape_string($common_name);
+        $short_name = $db->real_escape_string($short_name);
+
+        $add_query = "INSERT INTO Type (manf, number, common_name, short_name) VALUES (" .
+            "'" . $manf . "', " .
+            "'" . $number . "', " .
+            "'" . $common_name . "', " . 
+            "'" . $short_name . ");";
+
+        //this should be either true or false
+        return $db->query($add_query);
+    }
+
+    function set_manf($tid, $manf) {
+        global $db;
+        $manf = $db->real_escape_string($manf);
+        $set_query = "UPDATE Type SET manf='" . $manf . "' WHERE tid='" . $tid . "';";
+
+        return $db->query($set_query);
+    }
+
+    function set_number($tid, $number) {
+        global $db;
+        $manf = $db->real_escape_string($number);
+        $set_query = "UPDATE Type SET manf='" . $number . "' WHERE tid='" . $tid . "';";
+
+        return $db->query($set_query);
+    }
+
+    function set_common_name($tid, $common_name) {
+        global $db;
+        $manf = $db->real_escape_string($common_name);
+        $set_query = "UPDATE Type SET manf='" . $common_name . "' WHERE tid='" . $tid . "';";
+
+        return $db->query($set_query);
+    }
+
+    function set_short_name($tid, $short_name) {
+        global $db;
+        $manf = $db->real_escape_string($short_name);
+        $set_query = "UPDATE Type SET manf='" . $short_name . "' WHERE tid='" . $tid . "';";
+
+        return $db->query($set_query);
+    }
+
+
+?>
---
+++ typerecords.php
@@ -0,0 +1,82 @@
+<?php
+    include_once(__DIR__."/conf.php");
+    //I said I wouldn't do this, but I can't think of how else to best organize stuff
+    include_once(__DIR__."/db/types.php");
+
+    //change this condition
+    /*if (!isset($_POST['aircraft']) || (!isset($_POST['cost']) && !isset($_POST['delete']))) {
+        $_SESSION['record_err'] = "Something is horribly wrong, or you've forgotten to set a field.";
+        // Can safely do this, as the admin page will redirect to login if not admin
+        header('Location: '.SITE_ROOT.'/admin.php');
+    }*/
+    if (isset($_POST['delete'])) {
+        $tid = $_POST['tid'];
+        if (delete_type_record($tid)) {
+            $_SESSION['type_record_msg'] = "Deletion successful";
+        } else {
+            $_SESSION['type_record_err'] = "Deletion unsuccessful";
+        }
+    }
+    if (isset($_POST['change'])) {
+        $tid = $_POST['tid'];
+        $error = false;
+        if (isset($_POST['manf'])) {
+            if (!set_manf($tid, $_POST['manf'])) {
+                $error = true;
+            }
+        }
+        if (isset($_POST['number'])) {
+            if (!set_number($tid, $_POST['number'])) {
+                $error = true;
+            }
+        }
+        if (isset($_POST['common_name'])) {
+            if ($_POST['common_name'] == "NULL") {
+                $_POST['common_name'] = NULL;
+            }
+            if (!set_common_name($tid, $_POST['common_name'])) {
+                $error = true;
+            }
+        }
+        if (isset($_POST['short_name'])) {
+            if (!set_short_name($tid, $_POST['short_name'])) {
+                $error = true;
+            }
+        }
+        if ($error) {
+            $_SESSION['type_record_err'] = "Update unsuccessful";
+        } else {
+            $_SESSION['type_record_msg'] = "Update successful";
+        }
+    }
+    if (isset($_POST['add'])) {
+        $manf = $_POST['manf'];
+        $number = $_POST['number'];
+        $common_name = $_POST['common_name'];
+        $short_name = $_POST['short_name'];
+        if (add_type($manf, $number, $common_name, $short_name)) {
+            $_SESSION['type_add_msg'] = "Addition successful";
+        } else {
+            $_SESSION['type_add_err'] = "Addition unsuccessful";
+        }
+    }
+    header('Location: '.SITE_ROOT.'/admin.php');
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
