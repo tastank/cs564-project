2c0b9d1ece06ebff29fd31fd7e7ab723b4e154b2
Tyler Stank <tbear2500@gmail.com>
2015-05-03 20:07:09 -0500

    Implemented authorize_pilot()

--- db/auth.php
+++ db/auth.php
@@ -1,9 +1,23 @@
 <?php
+    include_once(__DIR__."/db.php");
     function authorize_pilot($pilot, $aircraft) {
+        global $db;
+        $pilot    = $db->real_escape_string($pilot);
+        $aircraft = $db->real_escape_string($aircraft);
+
         //prevent duplicates
-        $query = "SELECT * FROM can_rent WHERE username='" . $pilot . "' AND reg_number='" . $aircraft . "';";
-
-
+        $find_query = "SELECT * FROM can_rent WHERE username='" . $pilot . "' AND reg_number='" . $aircraft . "';";
+        $result = $db->query($find_query);
+        if ($result === false || $result->num_rows != 0) {
+            return;
+        }
+        $result->close();
+        $add_query = "INSERT INTO can_rent (username, reg_number) VALUES ('" . $pilot . "', '" . $aircraft . "');";
+        $result = $db->query($add_query);
+        if ($result === false) {
+            return false;
+        }
+        return true;
     }
 
 ?>
